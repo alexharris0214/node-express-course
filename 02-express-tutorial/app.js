cb128d0a959b5949005af3d3e137147f3419a9ad
allimg7142 <alexharris0214@gmail.com>
2022-06-28 18:49:58 -0400

    init

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,23 @@
-console.log('Express Tutorial')
+const http = require('http')
+const {readFileSync} = require('fs');
+
+// get all files
+const homePage = readFileSync('./navbar-app/index.html');
+
+
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    if(url == '/'){
+        res.writeHead(200, {'content-type': 'text/html'})
+        res.write(homePage)
+        res.end();
+    } else if (url == '/about'){
+        res.writeHead(200, {'content-type': 'text/html'});
+        res.write('<h1> about page </h1>');
+        res.end();
+    } else {
+        res.writeHead(200, 'about');
+    }
+});
+
+server.listen(5000);
